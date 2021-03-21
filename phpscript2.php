55107e2dcd8274f49eb2e4de10230d1e3f1f1e62
agbleze <62272559+agbleze@users.noreply.github.com>
2021-03-21 20:58:07 +0100

    Add files via upload

---
+++ phpscript2.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <?php
+            $name="John";
+            echo "Welcome $name <br/>";
+            $a=10;
+            $b=20;
+            echo "Sum of $a and $b is ";
+            echo $a+$b; 
+            echo 'I am now learning php <br/>';
+
+            echo "I am $b years old <br/>";
+
+            echo "This website " . "is our marketplace"
+        ?>
+    </body>
+
+</html>
